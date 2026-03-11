0298dd93771543a08a66c77acdfba325221179d9
commit-panda <sjmanyarkiy@gmail.com>
2026-03-11 16:57:18 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,41 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+
+    bakery_list = [bakery.to_dict() for bakery in bakeries]
+ 
+    response = make_response(bakery_list, 200)
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(bakery_dict, 200)
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+
+    bg_by_price = [ baked_good.to_dict() for baked_good in baked_goods]
+
+    response = make_response(bg_by_price, 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    exp_baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).first()
+
+    expensive_good = exp_baked_goods.to_dict()
+    response = make_response(expensive_good, 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
